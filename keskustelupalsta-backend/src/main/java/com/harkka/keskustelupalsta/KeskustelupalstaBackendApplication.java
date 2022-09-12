26305754ecd0b95a3289d6ef245bbf0fe864b271
Börje Backman <112690883+solita-borjeba@users.noreply.github.com>
2022-09-12 13:03:48 +0300

    Add files via upload

---
+++ keskustelupalsta-backend/src/main/java/com/harkka/keskustelupalsta/KeskustelupalstaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.harkka.keskustelupalsta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeskustelupalstaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeskustelupalstaBackendApplication.class, args);
+	}
+
+}
---
+++ keskustelupalsta-backend/src/test/java/com/harkka/keskustelupalsta/KeskustelupalstaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harkka.keskustelupalsta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeskustelupalstaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
